b6584e8193748fdc7157dbdd31baab72597f4118
programming4life <samuelheil@web.de>
2023-06-26 22:08:13 +0200

    Edited item IDs

--- src/main/kotlin/items/ItemManager.kt
+++ src/main/kotlin/items/ItemManager.kt
@@ -5,9 +5,9 @@ import org.bukkit.inventory.ItemStack
 
 class ItemManager{
     fun grapplingHook(): ItemStack { return TrollItem("grappling_hook", Material.FISHING_ROD, "Grappling Hook", "Travel in style...", "", TrollRarity.UNCOMMON).createItem() }
-    fun throwableTNT(): ItemStack { return TrollItem("grappling_hook", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.RARE, "TNT", true, null, true).createItem() }
+    fun throwableTNT(): ItemStack { return TrollItem("throwable_tnt", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.RARE, "TNT", true, null, true).createItem() }
     val items = listOf(
         TrollItem("grappling_hook", Material.FISHING_ROD, "Grappling Hook", "Travel in style...", "", TrollRarity.UNCOMMON).createItem(),
-        TrollItem("grappling_hook", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.RARE, "TNT", true, null, true).createItem()
+        TrollItem("throwable_tnt", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.RARE, "TNT", true, null, true).createItem()
     )
 }
