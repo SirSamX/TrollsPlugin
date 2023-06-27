b32114f6091b3a97d72825e4ea1849fbc90dff0d
SirSam <samuelheil@web.de>
2023-06-27 18:01:44 +0200

    Added Explosive Bow

--- src/main/kotlin/items/ItemManager.kt
+++ src/main/kotlin/items/ItemManager.kt
@@ -4,7 +4,8 @@ import org.bukkit.Material
 
 class ItemManager{
     val items = listOf(
-        TrollItem("grappling_hook", Material.FISHING_ROD, "Grappling Hook", "Travel in style...", "", TrollRarity.UNCOMMON),
-        TrollItem("throwable_tnt", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.RARE, "TNT", true, null, true)
+        TrollItem("grappling_hook", Material.FISHING_ROD, "Grappling Hook", "Travel in style...", "", TrollRarity.RARE),
+        TrollItem("throwable_tnt", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.UNCOMMON, "TNT", true, null, true),
+        TrollItem("explosive_bow", Material.BOW, "Explosive Bow", "Shoot to create an explosion.", "", TrollRarity.LEGENDARY, "Bow")
     )
 }
