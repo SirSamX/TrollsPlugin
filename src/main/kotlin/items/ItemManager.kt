abdcd539a55b95d76328517c833b4fb385e50eca
SirSam <samuelheil@web.de>
2023-06-27 17:01:48 +0200

    Removed unused item functions

--- src/main/kotlin/items/ItemManager.kt
+++ src/main/kotlin/items/ItemManager.kt
@@ -1,12 +1,8 @@
 package me.sirsam.trolls.items
 
 import org.bukkit.Material
-import org.bukkit.inventory.ItemStack
 
 class ItemManager{
-    fun grapplingHook(): ItemStack { return TrollItem("grappling_hook", Material.FISHING_ROD, "Grappling Hook", "Travel in style...", "", TrollRarity.UNCOMMON).createItem() }
-    fun throwableTNT(): ItemStack { return TrollItem("throwable_tnt", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.RARE, "TNT", true, null, true).createItem() }
-
     val items = listOf(
         TrollItem("grappling_hook", Material.FISHING_ROD, "Grappling Hook", "Travel in style...", "", TrollRarity.UNCOMMON),
         TrollItem("throwable_tnt", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.RARE, "TNT", true, null, true)
