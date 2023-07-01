50ea276250dbfbe7a3b8976a76f9c6af93d52ebe
SirSam <samuelheil@web.de>
2023-07-01 17:32:03 +0200

    Fixed Arguments

--- src/main/kotlin/commands/Sudo.kt
+++ src/main/kotlin/commands/Sudo.kt
@@ -13,7 +13,7 @@ class Sudo : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
         if (sender !is Player) { utils.isNotPlayerMessage(sender); return true }
         if (args!!.isEmpty()) { utils.formattingErrorMessage(sender); return true }
-        val target = Bukkit.getPlayer(args!![0])
+        val target = Bukkit.getPlayer(args[0])
         if (target == null) {
             Utilities().playerNotExistingMessage(sender)
             return true
