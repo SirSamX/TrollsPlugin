fa1b63336c209139b8331c49252b3344dc0cb393
SirSam <samuelheil@web.de>
2023-07-01 11:29:20 +0200

    Added Shooty Box

--- src/main/kotlin/items/ItemManager.kt
+++ src/main/kotlin/items/ItemManager.kt
@@ -10,6 +10,7 @@ class ItemManager{
         TrollItem("throwable_fireball", Material.FIRE_CHARGE, "Throwable Fireball", "Right-click to throw a Fireball.", null, TrollRarity.UNCOMMON, "FIREBALL", oneTimeUse = true),
         TrollItem("explosive_bow", Material.BOW, "Explosive Bow", "Shoot to create an explosion.", null, TrollRarity.LEGENDARY, "BOW"),
         //TrollItem("leap", Material.FEATHER, "Leap", "Right-click to launch you in the direction you're' looking.", rarity = TrollRarity.EPIC),         ### NOT WORKING BECAUSE OF LAGGING BACK WHEN USING ABILITY! ###
-        TrollItem("pogeroni_sword", Material.NETHERITE_SWORD, "Pogeroni Sword", "Fickt Emanuel.", "Hallo", TrollRarity.RARE, "SWORD", enchantments = mutableMapOf(Enchantment.DAMAGE_ALL to 5, Enchantment.DURABILITY to 3), unbreakable = false)
+        TrollItem("pogeroni_sword", Material.NETHERITE_SWORD, "Pogeroni Sword", "Fickt Emanuel.", "Hallo", TrollRarity.RARE, "SWORD", enchantments = mutableMapOf(Enchantment.DAMAGE_ALL to 5, Enchantment.DURABILITY to 3), unbreakable = false),
+        TrollItem("shooty_box", Material.DISPENSER, "Shooty Box", "Shift + Right-click to open GUI.\nRight click to shoot!", null, TrollRarity.UNFINISHED)
     )
 }
