4db92f37a3b3fde8fc831f41f1e742e3e6b9fb2c
SirSam <samuelheil@web.de>
2023-07-01 11:14:34 +0200

    Changed Rarity of throwable fireball tp uncommon

--- src/main/kotlin/items/ItemManager.kt
+++ src/main/kotlin/items/ItemManager.kt
@@ -7,9 +7,9 @@ class ItemManager{
     val items = listOf(
         TrollItem("grappling_hook", Material.FISHING_ROD, "Grappling Hook", "Travel in style...", null, TrollRarity.RARE),
         TrollItem("throwable_tnt", Material.TNT, "Throwable TNT", "Right-click to Throw TNT.", "Don't grief!", TrollRarity.UNCOMMON, "TNT", oneTimeUse = true),
-        TrollItem("throwable_fireball", Material.FIRE_CHARGE, "Throwable Fireball", "Right-click to throw a Fireball.", null, TrollRarity.UNFINISHED, "FIREBALL", oneTimeUse = true),
+        TrollItem("throwable_fireball", Material.FIRE_CHARGE, "Throwable Fireball", "Right-click to throw a Fireball.", null, TrollRarity.UNCOMMON, "FIREBALL", oneTimeUse = true),
         TrollItem("explosive_bow", Material.BOW, "Explosive Bow", "Shoot to create an explosion.", null, TrollRarity.LEGENDARY, "BOW"),
         //TrollItem("leap", Material.FEATHER, "Leap", "Right-click to launch you in the direction you're' looking.", rarity = TrollRarity.EPIC),         ### NOT WORKING BECAUSE OF LAGGING BACK WHEN USING ABILITY! ###
-        TrollItem("pogeroni_sword", Material.NETHERITE_SWORD, "Pogeroni Sword", "Fickt Emanuel.", "Hallo", TrollRarity.RARE, "SWORD", enchantments = mutableMapOf(Enchantment.DAMAGE_ALL to 10, Enchantment.DURABILITY to 5), unbreakable = false)
+        TrollItem("pogeroni_sword", Material.NETHERITE_SWORD, "Pogeroni Sword", "Fickt Emanuel.", "Hallo", TrollRarity.RARE, "SWORD", enchantments = mutableMapOf(Enchantment.DAMAGE_ALL to 5, Enchantment.DURABILITY to 3), unbreakable = false)
     )
 }
