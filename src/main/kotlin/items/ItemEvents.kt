434aa200c3ee1ac7965ab65bf485948297d1503c
SirSam <samuelheil@web.de>
2023-07-02 21:35:51 +0200

    Added Magical Wand

--- src/main/kotlin/items/ItemEvents.kt
+++ src/main/kotlin/items/ItemEvents.kt
@@ -3,15 +3,11 @@ package me.sirsam.trolls.items
 import me.sirsam.trolls.Trolls
 import me.sirsam.trolls.helpers.Utilities
 import net.kyori.adventure.text.Component
-import org.bukkit.Bukkit
-import org.bukkit.GameMode
-import org.bukkit.Material
-import org.bukkit.Particle
+import org.bukkit.*
 import org.bukkit.block.Block
 import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.Action
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.entity.EntityShootBowEvent
 import org.bukkit.event.entity.ProjectileHitEvent
@@ -21,7 +17,9 @@ import org.bukkit.event.player.PlayerFishEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.util.Vector
 import kotlin.math.cos
+import kotlin.math.exp
 import kotlin.math.sin
 
 
@@ -36,7 +34,11 @@ class ItemEvents : Listener {
         val data = item.itemMeta.persistentDataContainer
         val strength = .3
 
-        if (data.get(utils.idKey, PersistentDataType.STRING) == "grappling_hook" && event.state == PlayerFishEvent.State.REEL_IN) {
+        if (data.get(
+                utils.idKey,
+                PersistentDataType.STRING
+            ) == "grappling_hook" && event.state == PlayerFishEvent.State.REEL_IN
+        ) {
             val hookLocation = event.hook.location
             val change = hookLocation.subtract(player.location)
             player.velocity = change.toVector().multiply(strength)
@@ -48,13 +50,15 @@ class ItemEvents : Listener {
         val player = event.player
         val item = player.inventory.itemInMainHand
 
-        if(item.itemMeta != null) {
+        if (item.itemMeta != null) {
             val data = item.itemMeta.persistentDataContainer
 
             if (data.get(utils.idKey, PersistentDataType.STRING) == "throwable_tnt" && event.action.isRightClick) {
                 val strength = 1.4
                 event.isCancelled = true
-                if (player.gameMode != GameMode.CREATIVE) { utils.destroy(item, 1) }
+                if (player.gameMode != GameMode.CREATIVE) {
+                    utils.destroy(item, 1)
+                }
                 val direction = player.location.direction
                 val tntEntity = player.world.spawnEntity(player.location, EntityType.PRIMED_TNT)
                 tntEntity.velocity = direction.multiply(strength)
@@ -67,12 +71,22 @@ class ItemEvents : Listener {
         val player = event.player
         val item = player.inventory.itemInMainHand
         if (item.itemMeta != null) {
-            if (item.itemMeta.persistentDataContainer.get(utils.idKey, PersistentDataType.STRING) == "shooty_box" && event.action.isRightClick) {
+            if (item.itemMeta.persistentDataContainer.get(
+                    utils.idKey,
+                    PersistentDataType.STRING
+                ) == "shooty_box" && event.action.isRightClick
+            ) {
                 event.isCancelled = true
                 if (player.isSneaking) {
                     val inv = utils.getInventoryInItem(item)
                     if (inv == null) {
-                        player.openInventory(Bukkit.createInventory(null, InventoryType.DISPENSER, Component.text("Shooty Box")))
+                        player.openInventory(
+                            Bukkit.createInventory(
+                                null,
+                                InventoryType.DISPENSER,
+                                Component.text("Shooty Box")
+                            )
+                        )
                     } else {
                         player.openInventory(inv)
                     }
@@ -84,23 +98,29 @@ class ItemEvents : Listener {
     @EventHandler
     fun closeShootyBoxGui(event: InventoryCloseEvent) {
         val item = event.player.inventory.itemInMainHand
-        if (event.view.title().contains(Component.text("Shooty Box")) && item.itemMeta.persistentDataContainer.get(utils.idKey, PersistentDataType.STRING) == "shooty_box") {
+        if (event.view.title().contains(Component.text("Shooty Box")) && item.itemMeta.persistentDataContainer.get(
+                utils.idKey,
+                PersistentDataType.STRING
+            ) == "shooty_box"
+        ) {
             utils.storeInventoryInItem(item, event.inventory)
         }
     }
 
     @EventHandler
-    fun explosiveBowShoot(event: EntityShootBowEvent){
-        if (event.entity is Player){
-            val player = event.entity as Player
-            if (player.inventory.itemInMainHand.itemMeta.persistentDataContainer.get(utils.idKey, PersistentDataType.STRING) == "explosive_bow") {
-                event.projectile.customName(Component.text("Explosive Arrow"))
-            }
+    fun explosiveBowShoot(event: EntityShootBowEvent) {
+        val player = event.entity as Player
+        if (player.inventory.itemInMainHand.itemMeta.persistentDataContainer.get(
+                utils.idKey,
+                PersistentDataType.STRING
+            ) == "explosive_bow"
+        ) {
+            event.projectile.customName(Component.text("Explosive Arrow"))
         }
     }
 
     @EventHandler
-    fun explosiveBowHit(event: ProjectileHitEvent){
+    fun explosiveBowHit(event: ProjectileHitEvent) {
         if (event.entity.customName() != null) {
             if (event.entity.customName() == Component.text("Explosive Arrow")) {
                 event.entity.world.createExplosion(event.entity.location, 2.5f, false, true)
@@ -114,14 +134,19 @@ class ItemEvents : Listener {
         val player = event.player
         val item = player.inventory.itemInMainHand
 
-        if(item.itemMeta != null) {
+        if (item.itemMeta != null) {
             val data = item.itemMeta.persistentDataContainer
 
             if (data.get(utils.idKey, PersistentDataType.STRING) == "throwable_fireball" && event.action.isRightClick) {
                 val strength = 2
                 event.isCancelled = true
-                if (player.gameMode != GameMode.CREATIVE) { utils.destroy(item, 1) }
-                player.launchProjectile(LargeFireball::class.java, player.eyeLocation.direction.normalize().multiply(strength))
+                if (player.gameMode != GameMode.CREATIVE) {
+                    utils.destroy(item, 1)
+                }
+                player.launchProjectile(
+                    LargeFireball::class.java,
+                    player.eyeLocation.direction.normalize().multiply(strength)
+                )
             }
         }
     }
@@ -131,7 +156,7 @@ class ItemEvents : Listener {
         val player = event.player
         val item = player.inventory.itemInMainHand
 
-        if(item.itemMeta != null) {
+        if (item.itemMeta != null) {
             val data = item.itemMeta.persistentDataContainer
             if (event.action.isLeftClick && data.get(utils.idKey, PersistentDataType.STRING) == "terminator") {
                 val arrow = player.launchProjectile(Arrow::class.java)
@@ -175,9 +200,17 @@ class ItemEvents : Listener {
         )
 
         if (block.type in logMaterials) {
-            when (event.player.inventory.itemInMainHand.itemMeta.persistentDataContainer.get(utils.idKey, PersistentDataType.STRING)) {
-                "treecapitator" -> { breakTree(block, logMaterials, 50, 1L); event.isCancelled = true }
-                "jungle_axe" -> { breakTree(block, logMaterials, 25, 3L); event.isCancelled = true }
+            when (event.player.inventory.itemInMainHand.itemMeta.persistentDataContainer.get(
+                utils.idKey,
+                PersistentDataType.STRING
+            )) {
+                "treecapitator" -> {
+                    breakTree(block, logMaterials, 50, 1L); event.isCancelled = true
+                }
+
+                "jungle_axe" -> {
+                    breakTree(block, logMaterials, 25, 3L); event.isCancelled = true
+                }
             }
         }
     }
@@ -209,32 +242,122 @@ class ItemEvents : Listener {
         for ((index, block) in treeBlocks.withIndex()) {
             plugin.server.scheduler.runTaskLater(plugin, Runnable {
                 block.breakNaturally()
-                block.world.spawnParticle(Particle.VILLAGER_HAPPY, block.location, 1, 0.0, 0.0, 0.0, 0.0)
             }, delay * index)
         }
     }
 
-    @EventHandler
-    fun particle(event: PlayerInteractEvent) {
-        val player = event.player
-        if (event.action == Action.LEFT_CLICK_AIR) {
-            object : BukkitRunnable() {
-                var loc = player.location
-                var t = 0.0
-                var r = 2.0
-                override fun run() {
-                    t += Math.PI / 16
-                    val x: Double = r * cos(t)
-                    val y: Double = r * sin(t)
-                    val z: Double = r * sin(t)
-                    loc.add(x, y, z)
-                    player.world.spawnParticle(Particle.DRIP_LAVA, loc, 1)
-                    loc.subtract(x, y, z)
-                    if (t > Math.PI * 8) {
-                        cancel()
-                    }
+    private fun particle(loc: Location) {
+        object : BukkitRunnable() {
+            var t = 0.0
+            var r = 2.0
+            override fun run() {
+                t += Math.PI / 16
+                val x: Double = r * cos(t)
+                val y: Double = r * sin(t)
+                val z: Double = r * sin(t)
+                loc.add(x, y, z)
+                loc.world.spawnParticle(Particle.DRIP_LAVA, loc, 1, 0.0, 0.0, 0.0)
+                loc.subtract(x, y, z)
+                if (t > Math.PI * 8) {
+                    cancel()
                 }
-            }.runTaskTimer(plugin, 0, 1)
+            }
+        }.runTaskTimer(plugin, 0, 1)
+    }
+
+    private fun bigParticle(loc: Location) {
+        object : BukkitRunnable() {
+            var t = Math.PI / 4
+            override fun run() {
+                t += 0.1 * Math.PI
+                var theta = 0.0
+                while (theta <= 2 * Math.PI) {
+                    var x = t * cos(theta)
+                    var y = 2 * exp(-0.1 * t) * sin(t) + 1.5
+                    var z = t * sin(theta)
+                    loc.add(x, y, z)
+                    loc.world.spawnParticle(Particle.FIREWORKS_SPARK, loc, 1, 0.0, 0.0, 0.0)
+                    loc.subtract(x, y, z)
+                    theta += Math.PI / 64
+                    x = t * cos(theta)
+                    y = 2 * exp(-0.1 * t) * sin(t) + 1.5
+                    z = t * sin(theta)
+                    loc.add(x, y, z)
+                    loc.world.spawnParticle(Particle.SPELL_WITCH, loc, 1, 0.0, 0.0, 0.0)
+                    loc.subtract(x, y, z)
+                    theta += Math.PI / 32
+                }
+                if (t > 20) {
+                    cancel()
+                }
+            }
+        }.runTaskTimer(plugin, 0, 1)
+    }
+
+    fun shootParticle(loc: Location) {
+        object : BukkitRunnable() {
+            var t = 0.0
+            val direction: Vector = loc.direction.normalize()
+            override fun run() {
+                t += 0.5
+                val x: Double = direction.x * t
+                val y: Double = direction.y * t + 1.5
+                val z: Double = direction.z * t
+                loc.add(x, y, z)
+                loc.world.spawnParticle(Particle.SPELL_WITCH, loc, 1, 0.0, 0.0, 0.0)
+                loc.subtract(x, y, z)
+                if (t > 30) {
+                    cancel()
+                }
+            }
+        }.runTaskTimer(plugin, 0, 1)
+    }
+
+
+    @EventHandler
+    fun magicalWand(event: PlayerInteractEvent) {
+        val player = event.player
+        val item = player.inventory.itemInMainHand
+
+        if (item.itemMeta != null) {
+            val itemMeta = item.itemMeta
+            val data = itemMeta.persistentDataContainer
+
+            if (data.get(utils.idKey, PersistentDataType.STRING) == "magical_wand") {
+                val wandKey = NamespacedKey(plugin, "ability")
+
+                if (event.action.isRightClick) {
+                    val loc = player.location
+
+                    when (data.get(wandKey, PersistentDataType.STRING)) {
+                        "particle" -> {
+                            particle(loc)
+                            player.sendMessage("Casting particle ability")
+                        }
+                        "big_particle" -> {
+                            bigParticle(loc)
+                            player.sendMessage("Casting big particle ability")
+                        }
+                        "shoot" -> {
+                            shootParticle(loc)
+                            player.sendMessage("Casting shoot particle ability")
+                        }
+                        else -> {
+                            player.sendMessage("Invalid ability")
+                        }
+                    }
+                } else if (event.action.isLeftClick) {
+                    val newAbility = when (data.get(wandKey, PersistentDataType.STRING)) {
+                        "particle" -> "big_particle"
+                        "big_particle" -> "shoot"
+                        "shoot" -> "particle"
+                        else -> "particle"
+                    }
+                    data.set(wandKey, PersistentDataType.STRING, newAbility)
+                    item.itemMeta = itemMeta
+                    player.sendMessage("Changed ability to $newAbility")
+                }
+            }
         }
     }
 }
--- src/main/kotlin/items/ItemManager.kt
+++ src/main/kotlin/items/ItemManager.kt
@@ -11,8 +11,9 @@ class ItemManager{
         TrollItem("explosive_bow", Material.BOW, "Explosive Bow", "Shoot to create an explosion.", null, TrollRarity.LEGENDARY, "BOW"),
         TrollItem("pogeroni_sword", Material.NETHERITE_SWORD, "Pogeroni Sword", "Fickt Emanuel.", "Hallo", TrollRarity.RARE, "SWORD", enchantments = mutableMapOf(Enchantment.DAMAGE_ALL to 5, Enchantment.DURABILITY to 3), unbreakable = false),
         TrollItem("shooty_box", Material.DISPENSER, "Shooty Box", "Shift + Right-click to open GUI. Right click to shoot.", rarity = TrollRarity.UNFINISHED),
-        TrollItem("terminator", Material.BOW, "Terminator", "Left-click to shoot 5 arrows.", null, TrollRarity.MYTHIC, "BOW"),
+        TrollItem("terminator", Material.BOW, "Terminator", "Left-click to shoot 3 arrows.", null, TrollRarity.MYTHIC, "BOW"),
         TrollItem("treecapitator", Material.GOLDEN_AXE, "Treecapitator", "Break wood to cut down a whole tree.", "Blocks: 50, Delay: 1 Ticks", TrollRarity.EPIC, "AXE"),
-        TrollItem("jungle_axe", Material.WOODEN_AXE, "Jungle Axe", "Break wood to cut down a whole tree.", "Blocks: 25, Delay: 3 Tick", TrollRarity.UNCOMMON, "AXE")
+        TrollItem("jungle_axe", Material.WOODEN_AXE, "Jungle Axe", "Break wood to cut down a whole tree.", "Blocks: 25, Delay: 3 Tick", TrollRarity.UNCOMMON, "AXE"),
+        TrollItem("magical_wand", Material.BLAZE_ROD, "Magical Wand", "Left-click to switch modes. Right-click to cast spell.", "TestItem", TrollRarity.ADMIN, "WAND")
     )
 }
