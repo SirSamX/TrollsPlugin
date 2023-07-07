d4223462a44afe7e29ca0ee9d5b3c3ccfc7f28c0
SirSam <samuelheil@web.de>
2023-07-07 22:30:29 +0200

    Added Ability system (unfinished)

--- src/main/kotlin/items/TrollItem.kt
+++ src/main/kotlin/items/TrollItem.kt
@@ -2,6 +2,7 @@ package me.sirsam.trolls.items
 
 import me.sirsam.trolls.helpers.Utilities
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemFlag
@@ -18,24 +19,24 @@ class TrollItem(
     private val raritySuffix: String = "",
     private val stackable: Boolean = true,
     private val enchantments: MutableMap<Enchantment, Int>? = null,
-    private val abilities: Array<String>? = null,
+    private val abilities: Map<TrollAbility, String>? = null,
     private val oneTimeUse: Boolean = false,
-    private val unbreakable: Boolean = true
+    private val unbreakable: Boolean = true,
+    private val customModelData: Int? = null
 ) {
     private val utils = Utilities()
 
     fun createItem(): ItemStack {
         val item = ItemStack(material)
         val meta = item.itemMeta
-        val formattedName = rarity.color + name
 
         meta.persistentDataContainer.set(utils.idKey, PersistentDataType.STRING, id)
-        meta.displayName(Component.text(formattedName))
+        meta.displayName(getNameComponent())
         meta.lore(addLore())
         meta.isUnbreakable = unbreakable
         meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE)
         if (enchantments != null) { item.addUnsafeEnchantments(enchantments) }
-        //item.itemMeta.setCustomModelData()
+        if (customModelData != null) { item.itemMeta.setCustomModelData(customModelData) }
         item.itemMeta = meta
 
         return item
@@ -43,13 +44,14 @@ class TrollItem(
 
     fun getId(): String { return id }
 
-    fun getName(color: Boolean = true): String {
-        return if (color) {
-            rarity.color + name
-        }
-        else name
-    }
+    fun getName(): String { return name }
 
+    fun getNameComponent(colored: Boolean = true): Component {
+        return if (colored) {
+            Component.text(name, rarity.color)
+        }
+        else Component.text(name)
+    }
 
     private fun addLore(): MutableList<Component> {
         val lore = mutableListOf<Component>()
@@ -60,23 +62,39 @@ class TrollItem(
             lore.add(Component.text(""))
         }
 
-        if (!(description.isNullOrBlank())) {
-            lore.add(Component.text("§7$description"))
+        if (!description.isNullOrBlank()) {
+            lore.add(Component.text("$description", NamedTextColor.GRAY))
             lore.add(Component.text(""))
         }
 
-        if (!(note.isNullOrBlank())) {
-            lore.add(Component.text("§8$note"))
+        if (!abilities.isNullOrEmpty()) {
+            for (ability in abilities) {
+                val type = ability.key.toString().replace("_", " ")
+                val name = ability.value
+                when (ability.key) {
+                    TrollAbility.RIGHT_CLICK -> { lore.add(Component.text("Item Ability: $name ", NamedTextColor.GOLD).append(Component.text(type, NamedTextColor.YELLOW))) }
+                    TrollAbility.LEFT_CLICK -> { lore.add(Component.text("Item Ability: $name ", NamedTextColor.GOLD).append(Component.text(type, NamedTextColor.YELLOW))) }
+                    TrollAbility.SHIFT_RIGHT_CLICK -> { lore.add(Component.text("Item Ability: $name ", NamedTextColor.GOLD).append(Component.text(type, NamedTextColor.YELLOW))) }
+                    TrollAbility.SHIFT_LEFT_CLICK -> { lore.add(Component.text("Item Ability: $name ", NamedTextColor.GOLD).append(Component.text(type, NamedTextColor.YELLOW))) }
+                    TrollAbility.FULL_SET_BONUS -> { lore.add(Component.text("Set Bonus: $name ", NamedTextColor.GOLD).append(Component.text(type, NamedTextColor.YELLOW))) }
+                    TrollAbility.PIECE_BONUS -> { lore.add(Component.text("Piece Bonus: $name ", NamedTextColor.GOLD).append(Component.text(type, NamedTextColor.YELLOW))) }
+                }
+            }
+            lore.add(Component.text(""))
+        }
+
+        if (!note.isNullOrBlank()) {
+            lore.add(Component.text("$note", NamedTextColor.DARK_GRAY))
             lore.add(Component.text(""))
 
         }
 
         if (oneTimeUse) {
-            lore.add(Component.text("§8(consumed on use)"))
+            lore.add(Component.text("(consumed on use)", NamedTextColor.DARK_GRAY))
             lore.add(Component.text(""))
         }
 
-        lore.add(Component.text(rarity.color + rarity + " " + raritySuffix.uppercase()))
+        lore.add(Component.text(rarity.toString().replace("_", " ") + " " + raritySuffix.uppercase(), rarity.color))
 
         return lore
     }
