14a3553e0eba4e79e8f90567732f57a36ee08545
EnderMo23 <fischer.moritz09@gmail.com>
2023-07-14 14:59:35 +0200

    Kick for Emma

--- src/main/kotlin/listeners/OnJoin.kt
+++ src/main/kotlin/listeners/OnJoin.kt
@@ -62,7 +62,7 @@ class OnJoin : Listener {
             }
             "hbjju" -> {
                 event.joinMessage(Component.text("Tabeas dummer Bruder is da!", NamedTextColor.YELLOW))
-                p.kick()
+                p.kick(Component.text("Du Huso!"))
             }
             else ->  event.joinMessage(Component.text("+ ", NamedTextColor.GREEN).append(Component.text("${p.name} moin bein neu hier oder spiele nicht so aktiv!", NamedTextColor.GOLD)))
         }
