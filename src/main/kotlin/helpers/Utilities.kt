ac9ddfe763943a7b74cf5abd98ae074250695df8
SirSam <samuelheil@web.de>
2023-09-16 23:24:08 +0200

    Added Utils for PlayerHeads and Player Profiles

--- src/main/kotlin/helpers/Utilities.kt
+++ src/main/kotlin/helpers/Utilities.kt
@@ -1,16 +1,26 @@
 package me.sirsam.trolls.helpers
 
+import com.destroystokyo.paper.profile.PlayerProfile
 import me.sirsam.trolls.Trolls
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.inventory.meta.SkullMeta
 import org.bukkit.persistence.PersistentDataType
+import org.bukkit.profile.PlayerTextures
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
 import kotlin.random.Random
 
+@Suppress("MemberVisibilityCanBePrivate")
 class Utilities {
     private val plugin = Trolls.getPlugin()
     private val config = plugin.config
@@ -21,19 +31,33 @@ class Utilities {
     val mobKey = NamespacedKey(plugin, "trollMob")
     val uuidKey = NamespacedKey(plugin, "uuid")
 
-    fun isTrollItem(i: ItemStack): Boolean { return i.itemMeta.persistentDataContainer.get(idKey, PersistentDataType.STRING) !== null }
+    fun isTrollItem(i: ItemStack): Boolean {
+        return i.itemMeta.persistentDataContainer.get(idKey, PersistentDataType.STRING) !== null
+    }
 
-    fun receiveItemMessage(p: Player, a: Int, i: Component) { p.sendMessage(Component.text("You received $a ").append(i).append(Component.text("!"))) }
+    fun receiveItemMessage(p: Player, a: Int, i: Component) {
+        p.sendMessage(Component.text("You received $a ").append(i).append(Component.text("!")))
+    }
 
-    fun formattingErrorMessage(p: Player) { p.sendMessage(Component.text("Formatting error, check your command syntax!", NamedTextColor.RED)) }
+    fun formattingErrorMessage(p: Player) {
+        p.sendMessage(Component.text("Formatting error, check your command syntax!", NamedTextColor.RED))
+    }
 
-    fun notPlayerMessage(p: CommandSender) { p.sendMessage(Component.text("Sender is not a player.")) }
+    fun notPlayerMessage(p: CommandSender) {
+        p.sendMessage(Component.text("Sender is not a player."))
+    }
 
-    fun playerNotExistingMessage(p: CommandSender) { p.sendMessage(Component.text("This player does not exist!", NamedTextColor.RED)) }
+    fun playerNotExistingMessage(p: CommandSender) {
+        p.sendMessage(Component.text("This player does not exist!", NamedTextColor.RED))
+    }
 
-    fun noPermissionMessage(p: CommandSender) { p.sendMessage(Component.text("You don't have permission to do that!", NamedTextColor.RED)) }
+    fun noPermissionMessage(p: CommandSender) {
+        p.sendMessage(Component.text("You don't have permission to do that!", NamedTextColor.RED))
+    }
 
-    fun isNumeric(input: String): Boolean { return input.toDoubleOrNull() != null }
+    fun isNumeric(input: String): Boolean {
+        return input.toDoubleOrNull() != null
+    }
 
     fun destroy(item: ItemStack, quantity: Int) {
         if (item.amount <= quantity) {
@@ -57,5 +81,40 @@ class Utilities {
     fun getInventoryInItem(item: ItemStack): Inventory? {
         val inventoryData = item.itemMeta.persistentDataContainer.get(compactInventoryKey, PersistentDataType.STRING)
         return BukkitSerialization().fromBase64(inventoryData)
+    }
+
+    fun getProfile(url: String): PlayerProfile {
+        val profile: PlayerProfile = Bukkit.createProfile(UUID.randomUUID())
+        val textures: PlayerTextures = profile.textures
+        val urlObject: URL
+        try {
+            urlObject = URL(url)
+        } catch (exception: MalformedURLException) {
+            throw RuntimeException("Invalid URL", exception)
+        }
+        textures.skin = urlObject
+        profile.setTextures(textures)
+        return profile
+    }
+
+    fun getProfileById(id: String): PlayerProfile {
+        return getProfile("http://textures.minecraft.net/texture/$id")
+    }
+
+    fun createSkull(id: String): ItemStack {
+        val head = ItemStack(Material.PLAYER_HEAD)
+        val headMeta = head.itemMeta as SkullMeta
+        headMeta.playerProfile = Utilities().getProfileById(id)
+        head.setItemMeta(headMeta)
+        return head
+    }
+
+    fun setTexture(skullMeta: SkullMeta, id: String): SkullMeta {
+        skullMeta.playerProfile = Utilities().getProfileById(id)
+        return skullMeta
+    }
+
+    fun setTexture(meta: ItemMeta, id: String): SkullMeta {
+        return setTexture(meta as SkullMeta, id)
     }
 }
